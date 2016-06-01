09d0eb11f9f4a6fc5c86c53490c191d6a727d311
张德荣 <zdrjson@gmail.com>
2016-06-01 16:02:10 +0800

    add mergeVideo

---
+++ MergeVideo/MergeVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MergeVideo
+//
+//  Created by 张德荣 on 16/6/1.
+//  Copyright © 2016年 zdrjson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MergeVideo/MergeVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MergeVideo
+//
+//  Created by 张德荣 on 16/6/1.
+//  Copyright © 2016年 zdrjson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
